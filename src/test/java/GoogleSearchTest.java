e7f608c8f705cae4311e3efee93596595a5785b3
OlgaCasian <casianolga@gmail.com>
2020-06-10 15:28:10 -0500

    Implement new scenario, change to class attribute, work on change from sleep to wait.

--- src/test/java/GoogleSearchTest.java
+++ src/test/java/GoogleSearchTest.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.Test;
 
 import static java.lang.Thread.sleep;
@@ -10,6 +11,17 @@ import static java.lang.Thread.sleep;
 public class GoogleSearchTest {
 
     private WebDriver driver;
+    private String precedingXpath;
+
+    @AfterSuite
+    public void tearDown() {
+        try {
+            sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.quit();
+    }
 
     /*
            -go to www.google.com page
@@ -24,6 +36,8 @@ public class GoogleSearchTest {
         String inputAddress = "https://www.google.com/";
         String queryString = "Automation in Test";
         String cssSelectorValue = ".gLFyf";
+        precedingXpath = "//*[@type='submit' and @value='Google Search']/preceding::*[@type='text']";
+
         //WebDriver driver;
 
         submitRequest(inputAddress, queryString, cssSelectorValue);
@@ -33,7 +47,8 @@ public class GoogleSearchTest {
     public void submitRequest(String inputAddress, String queryString, String cssSelectorValue) {
         driver = new FirefoxDriver();
         driver.get(inputAddress);
-        WebElement textInput = driver.findElement(By.cssSelector(cssSelectorValue));
+        //WebElement textInput = driver.findElement(By.cssSelector(cssSelectorValue)); todo: change this to XPath
+        WebElement textInput = driver.findElement(By.xpath(precedingXpath));
         textInput.sendKeys(queryString);
         textInput.submit();
 
